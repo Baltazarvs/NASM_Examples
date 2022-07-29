6c40c2b4acb68dbd3082648b30065b721438f190
baltazarvs <ilijafserbia123@gmail.com>
2022-07-30 01:16:30 +0200

    NASM x86 examples.

---
+++ CMPCoparingTest.asm
@@ -0,0 +1,64 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; Created by Baltazarus
+; NASM_Examples
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+global _start
+
+section .data
+	cmsg: db "Checking if ecx is less than 30", 0xA, 0x0
+	clen: equ $ - cmsg
+	
+	gmsg: db "ecx value is greater than 30", 0xA, 0x0
+	glen: equ $ - gmsg
+
+	lmsg: db "ecx is less than 30", 0xA, 0x0
+	llen: equ $ - lmsg	
+
+	emsg: db "ecx is equal to 30", 0xA, 0x0
+	elen: equ $ - emsg
+	
+section .text
+_start:
+	mov eax, 4
+	mov ebx, 1
+	mov ecx, cmsg
+	mov edx, clen
+	int 0x80
+	
+	mov ecx, 29		; TODO: Change this value to any you want.
+	cmp ecx, 30
+	je print_je		; If ecx is equal to 30
+	jle print_jle   ; If ecx is less than 30
+	
+	mov eax, 4
+	mov ebx, 1
+	mov ecx, gmsg
+	mov edx, glen
+	int 0x80
+	
+	jmp exit_it
+
+print_je:
+	mov eax, 4
+	mov ebx, 1
+	mov ecx, emsg
+	mov edx, elen
+	int 0x80
+
+	jmp exit_it
+	
+print_jle:
+	mov eax, 4
+	mov ebx, 1
+	mov ecx, lmsg
+	mov edx, llen
+	int 0x80
+	
+	jmp exit_it
+	
+exit_it:
+	mov eax, 1
+	mov ebx, 0
+	int 0x80
+
---
+++ UserInputTest.asm
@@ -0,0 +1,47 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; Created by Baltazarus
+; NASM_Examples
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+global _start
+
+section .data
+	msg: db "Enter something: ", 0x0
+	len: equ $ - msg
+
+	msg_o: db "You entered: ", 0x0
+	len_o: equ $ - msg_o
+
+
+section .bss
+	buff: resb 0x1E
+	
+section .text
+_start:
+	mov eax, 4		; sys_write
+	mov ebx, 1		; stdout
+	mov ecx, msg	; msg's source
+	mov edx, len	; msg's len
+	int 0x80
+	
+	mov eax, 3		; sys_read
+	mov ebx, 0		; stdin
+	mov ecx, buff	; buffer buff
+	mov edx, 0x1E	; buff's size (30 bytes)
+	int 0x80
+	
+	mov eax, 4
+	mov ebx, 1
+	mov ecx, msg_o
+	mov edx, len_o
+	int 0x80
+
+	mov eax, 4
+	mov ebx, 1
+	mov ecx, buff
+	mov edx, 0x1E
+	int 0x80
+	
+	mov eax, 1
+    mov ebx, 0
+    int 0x80
