abc69f495636bb5ba13405ea32cb1a324940543e
Baltazarus <93092559+Baltazarvs@users.noreply.github.com>
2022-07-30 14:20:33 +0200

    Update PrintNameTenTimes.asm

--- PrintNameTenTimes.asm
+++ PrintNameTenTimes.asm
@@ -9,7 +9,7 @@ section .bss
 
 section .text
 _start:
-    mov eax, 4      ; sys_write
+    	mov eax, 4      ; sys_write
 	mov ebx, 1      ; stdout
 	mov ecx, name   ; name's instruction message (Enter your name: )
 	mov edx, nlen   ; size of name
@@ -22,8 +22,8 @@ _start:
 	int 0x80
 	
 	mov ecx, 1      ; Assign the counter an initial value of 1 (i = 1)
-    push ecx        ; Push to the stack ecx, with the 0 as initial value.
-    jmp loop_it     ; Run the loop;
+    	push ecx        ; Push to the stack ecx, with the 0 as initial value.
+    	jmp loop_it     ; Run the loop;
 
 loop_it:
 	mov eax, 4      ; sys_write
@@ -32,15 +32,15 @@ loop_it:
 	mov edx, 0x1E   ; buffer size
 	int 0x80
 	
-    pop ecx         ; Pop from the stack ecx
-    cmp ecx, 10     ; Check ecx, you can set this value to any you want (suggested to use > 5)
-    je exit_it      ; if ecx is greater than 5, jump to exit_it. Change jg if you know what you are doing. :)
+    	pop ecx         ; Pop from the stack ecx
+    	cmp ecx, 10     ; Check ecx, you can set this value to any you want (suggested to use > 5)
+    	je exit_it      ; if ecx is greater than 5, jump to exit_it. Change jg if you know what you are doing. :)
     
-    inc ecx         ; Increase counter (ecx)
-    push ecx        ; Push to the stack ecx
-    jmp loop_it     ; Restart the loop. continue;
+    	inc ecx         ; Increase counter (ecx)
+    	push ecx        ; Push to the stack ecx
+    	jmp loop_it     ; Restart the loop. continue;
 
 exit_it:
-    mov eax, 1      ; sys_exit
-    mov ebx, ecx    ; return ecx
-    int 0x80        ; syscall
+    	mov eax, 1      ; sys_exit
+    	mov ebx, ecx    ; return ecx
+    	int 0x80        ; syscall
